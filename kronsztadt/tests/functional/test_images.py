5f8799a7034233f5a79cab0abdf6aac721417dd6
Szymon Pyżalski <zefciu@gmail.com>
2011-02-02 18:35:16 +0100

    First working version

--- kronsztadt/tests/functional/test_images.py
+++
@@ -1,7 +0,0 @@
-from kronsztadt.tests import *
-
-class TestImagesController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='images', action='index'))
-        # Test response...
